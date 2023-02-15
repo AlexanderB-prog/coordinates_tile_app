11f50aa4c69db11f26af281046e7ab905d5208b1
Alexander <121059969+AlexanderB-prog@users.noreply.github.com>
2023-02-15 13:17:42 +0300

    подключен firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:coordinates_tile/calculation_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'main/main_view.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
